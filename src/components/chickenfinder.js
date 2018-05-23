aaa5759e5f72b53e5634ddaaf9aaf7e127bd7b28
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-05-22 19:32:12 -0700

    added comments

--- src/components/chickenfinder.js
+++ src/components/chickenfinder.js
@@ -5,33 +5,43 @@ import ChickenColor from './chickencolor';
 class ChickenFinder extends Component {
   constructor(props){
     super(props);
-    this.state = {
+    this.state = { //store the values we need in this parent component
     	height: null,
     	color: null,
     }
+    //bind out click handlers and resethandlers so we don't lose "this"
     this.handleChildClick = this.handleChildClick.bind(this);
     this.resetChicken = this.resetChicken.bind(this);
   }
+  //this function is called by its children via a callback
   handleChildClick( componentName, value){
     console.log('child was clicked');
     console.log(`setting ${componentName} to ${value}`)
     this.setState({
-      [componentName]: value
+      [componentName]: value //the [componentName] lets you programmatically select a property name
     })
+    /*you also could have made the object like this
+    const obj = {};
+    obj[componentName] = value;
+    this.setState(obj);
+    */
   }
   resetChicken(){
     this.setState({
       height: null,
       color: null
     })
+    //change everything back to blank
   }
   render() {
 
-    if(this.state.height===null){
-      return (<ChickenHeight callback={this.handleChildClick}/>)
-    } else if(this.state.color===null){
-      return (<ChickenColor callback={this.handleChildClick}/>)
+    if(this.state.height===null){//if the first value is null, render the component to ask for the first value
+      return (<ChickenHeight callback={this.handleChildClick}/>) //pass the click handler to the child component
+    } else if(this.state.color===null){ //if the second value is null, render the component to ask fo the second value
+      return (<ChickenColor callback={this.handleChildClick}/>) 
     } else {
+      //we have all the pieces we need.  render something to show it.
+      //if you didn't need to render something, but wanted to call another function, you could a check in the componentDidUpdate lifecycle method
       return (
       <div>
         <div>You have made a {this.state.height} {this.state.color} chicken</div>
--- src/components/chickenheight.js
+++ src/components/chickenheight.js
@@ -3,8 +3,9 @@ import React from 'react';
 const ChickenHeight = (props) =>{
 	function handleClick( event){
 		console.log('handled click');
-		props.callback('height', event.currentTarget.innerText);
+		props.callback('height', event.currentTarget.innerText); //call the callback functiom from the parent via the props, give it our current component and the value that was clicked
 	}
+	//this renders out each choice. we pass the event into the local click handler so that it can be used to get the data from the element from the event
 	return (<div>
 				Pick a chicken height:
 				<div onClick={ (event) => { handleClick(event) } }>3ft</div>
